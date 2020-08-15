a099008d98f1833bb0628e3322b5bd4ec768e8d7
caot <>
2020-08-15 14:37:15 -0400

    https://spring.io/guides/gs/uploading-files/ --> Create an Application Class

--- src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
+++ src/main/java/com/example/uploadingfiles/UploadingFilesApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileUpload {
+public class UploadingFilesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FileUpload.class, args);
+        SpringApplication.run(UploadingFilesApplication.class, args);
     }
-
 }
--- src/test/java/com/example/uploadingfiles/UploadingFilesApplicationTests.java
+++ src/test/java/com/example/uploadingfiles/UploadingFilesApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileUploadTests {
+class UploadingFilesApplicationTests {
 
     @Test
     void contextLoads() {
